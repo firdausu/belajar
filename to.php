09de388550209fc1eb2100b1f0c6866c9ae45031
firdausu <109668656+firdausu@users.noreply.github.com>
2023-10-12 23:18:12 +0700

    Create to.php

---
+++ to.php
@@ -0,0 +1,214 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+define('TOKEN_BOT', '6406839120:AAGTxZ1XkvFcJ13xPD0OmFPjy664_01U9ss');
+function uuidv4() {
+    $data = openssl_random_pseudo_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); 
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); 
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+function curl($url, $headers, $mode="get", $data=0)
+        {
+        if ($mode == "get" || $mode == "Get" || $mode == "GET")
+                {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                $result = curl_exec($ch);
+                }
+        elseif ($mode == "post" || $mode == "Post" || $mode == "POST")
+                {
+                $headers[] = "Content-Length: ".strlen($data);
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                $result = curl_exec($ch);
+                }
+        else
+                {
+                $result = "Not define";
+                }
+        return $result;
+}
+function sendTelegramMessage($chatId, $message) {
+    $url = "https://api.telegram.org/bot" . TOKEN_BOT . "/sendMessage";
+    $postData = http_build_query(array(
+        'chat_id' => $chatId,
+        'text' => $message
+    ));
+
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => 'Content-Type: application/x-www-form-urlencoded',
+            'content' => $postData
+        )
+    );
+
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    return $result;
+}
+function decrypt($text,$key,$iv){
+return openssl_decrypt(base64_decode($text), 'aes-128-cbc', $key, OPENSSL_RAW_DATA, $iv);
+}
+function encrypt($text,$key,$iv){
+return base64_encode(openssl_encrypt($text, 'aes-128-cbc', $key, OPENSSL_RAW_DATA, $iv));
+}
+function random($i){
+    $characters = "abcdef0123456789";
+    $length = strlen($characters);
+    $randomString = "";
+
+    for ($i2 = 0; $i2 < $i; $i2++) {
+        $randomString .= $characters[rand(0, $length - 1)];
+    }
+
+    return $randomString;
+}
+function timer($waktu){
+for ($x=$waktu;$x>0;$x--){
+echo "\r                                             \r";
+echo "Please wait ".$x." second ";
+sleep(1);
+echo "\r                                             \r";
+}
+}
+$secretkey = md5(chr(104) . chr(51) . chr(94) . chr(37) . chr(52) . chr(126) . chr(115) . chr(120) . chr(106) . chr(108) . chr(115) . chr(100) . chr(57) . chr(49), true);
+$iv = chr(0) . chr(1) . chr(2) . chr(3) . chr(4) . chr(5) . chr(6) . chr(7) . chr(8) . chr(9) . chr(10) . chr(11) . chr(12) . chr(13) . chr(14) . chr(15);
+
+$uuid = uuidv4();
+$phone = "89609972692";
+$token = $uuid;
+$token = uuidv4();
+$anid = random(16);
+$akun = [
+[
+"a" => "88af8d09-3dd9-4881-bd6e-a1e83fad1b89",
+"b" => "d60f3beb81b124f5"
+],
+[
+"a" => "a215a738-3bc4-41f7-9fe6-5c1b7781b849",
+"b" => "36cfadb1e41ed5cb"
+],
+[
+"a" => "42d6acc2-ded3-4f44-b49b-36f0ccd5a3ea",
+"b" => "1a001db924f51de4"
+],
+[
+"a" => "65b9301e-ba17-4a0a-b8da-f76ec33c2775",
+"b" => "a8e598605b617414"
+],
+[
+"a" => "1c12422e-abf8-4b7d-9842-62a519821781",
+"b" => "6c9bf63756f82261"
+],
+[
+"a" => "b6b4acb3-151b-4b1d-b3c6-302025aed2cc",
+"b" => "9a4acec957fa65a1"
+],
+[
+"a" => "9c399b42-e21c-44b7-9dc0-fc6e96a66b07",
+"b" => "11cbba067a7deccb"
+],
+[
+"a" => "67ce6d85-5f5e-4c62-a886-8f96459b5a95",
+"b" => "1198b4d19ed0466c"
+],
+];
+$xx = 1;
+while(true){
+foreach($akun as $in => $aa){
+$uuid = uuidv4();
+$anid = $aa["b"];
+$token = $aa["a"];
+$headers = [
+'User-Agent: Android Client',
+'Content-Type: application/json;charset=utf-8',
+'token: '.$token,
+'X-Unity-Version: 2021.3.18f1c1'
+];
+//echo "[$in] Akun => $token\n";
+$data = encrypt('{"location":"ID","macAddress":"","uuid":"'.$token.'","advertisingId":"'.$token.'","product":0,"app_version":"1.1.8","user_channel":"knife","distinct_id":"'.$token.'","deviceId":"'.$token.'","imei":"","anid":"'.$anid.'","oaid":"","idfa":"","idfv":"","status_code":0,"wxTime":0,"country":"ID","version":118,"channel":"knife","sdkName":"10","sdkVersion":29,"isEmulator":0,"isVirtual":0,"isDevelperMode":0,"isRoot":0,"carrierName":"3","carrierCode":"51089","isOpenVpn":0}',$secretkey,$iv);
+$balance = json_decode(curl('https://game.firstwinner.org/api/app/fc/user/start?'.round(microtime(true) * 1000),$headers,'POST',$data));
+$dec = decrypt($balance,$secretkey,$iv);
+$json = json_decode($dec);
+//var_dump($json);
+if($json->code == 200){
+$total = $json->data->total;
+if($total == 0){
+	}else{
+echo "[$xx][$in] Akun => $token | $total"."\n";
+}
+
+
+if($total >= 1000){
+$data = encrypt('{"phone":"'.$phone.'","country":"ID","version":118,"channel":"knife","sdkName":"10","sdkVersion":29,"isEmulator":0,"isVirtual":0,"isDevelperMode":0,"isRoot":0,"carrierName":"3","carrierCode":"51089","isOpenVpn":0}',$secretkey,$iv);
+$paylist = json_decode(curl('https://game.firstwinner.org/api/app/fc/pay/coin/paylist?'.round(microtime(true) * 1000),$headers,'POST',$data));
+$dec2 = decrypt($paylist,$secretkey,$iv);
+$json = json_decode($dec2);
+if($json->code == 200){
+foreach($json->data as $payliss){
+if($total >= $payliss->amount && $payliss->last > 0){
+$data = encrypt('{"mail":"bagelgaxxng2003@gmail.com","phone":"'.$phone.'","name":"Asep","amount":'.$payliss->amount.',"country":"ID","currency":"IDR","payment":"DANA","version":118,"channel":"knife","sdkName":"10","sdkVersion":29,"isEmulator":0,"isVirtual":0,"isDevelperMode":0,"isRoot":0,"carrierName":"3","carrierCode":"51089","isOpenVpn":0}',$secretkey,$iv);
+$person = json_decode(curl('https://game.firstwinner.org/api/app/fc/pay/coin/person?'.round(microtime(true) * 1000),$headers,'POST',$data));
+$dec3 = decrypt($person,$secretkey,$iv);
+$json = json_decode($dec3);
+if($json->code == 200){
+$chatId = "1397941074";
+$message = "===================\nAndroid ID: $anid\nToken: $token\n"."WD: ".$json->data->credit."\n===================\n";
+$response = sendTelegramMessage($chatId, $message);
+if ($response === false) {
+    echo "Pesan tidak terkirim.\n";
+} else {
+    echo "Pesan berhasil terkirim.\n";
+}
+echo "[$in] Credit: ".$json->data->credit." | Status: ".$json->data->status." | Text: ".$json->data->text." | Sisa Balance: ".$json->data->total."\n";
+}else{
+echo "Withdraw: ".$dec3."\n";
+}//person
+}//kondisi
+}//foreach
+}else{
+echo "Pay List: ".$dec2."\n";
+}
+}
+
+
+}else{
+echo "Check Balance: ".$dec."\n";
+}
+
+
+$data = encrypt('{"pkgName":"com.knife.game.happy","imei":"","oaid":"","androidId":"'.$token.'","idfa":"","idfv":"","adKey":"'.random(14).'","adprice":0.999999,"adPlatform":66,"sign":"'.$token.round(microtime(true) * 1000).'","status_code":0,"from":1,"country":"ID","adType":'.rand(9,18).',"version":118,"channel":"knife","sdkName":"10","sdkVersion":29,"isEmulator":0,"isVirtual":0,"isDevelperMode":0,"isRoot":0,"carrierName":"3SinyalKuatHemat","carrierCode":"51089","isOpenVpn":0}',$secretkey,$iv);
+$reward = json_decode(curl('https://game.firstwinner.org/api/app/fc/credit/coin?'.round(microtime(true) * 1000),$headers,'POST',$data));
+$decs = decrypt($reward,$secretkey,$iv);
+$json = json_decode($decs);
+//var_dump($json);
+if($json->code == 200){
+echo "[$xx][$in] Claim Reward: OK - ".$json->data->credit."\n";
+}else{
+echo "Claim Reward: ".$decs."\n";
+}
+//timer(1);
+
+
+
+}
+$xx++;
+if($xx % 100 == 0){
+	echo exec("clear");
+//	banner();
+}
+}
+
+
